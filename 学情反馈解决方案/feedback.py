711958a031201d0cf242927494e5cc663851e317
campanulata <ts1989512@outlook.com>
2020-01-17 20:47:16 +0800

    1

--- 学情反馈解决方案/feedback.py
+++ 学情反馈解决方案/feedback.py
@@ -26,7 +26,7 @@ def feedback(time_live, grade, name):
     else:
         str2 = '然后作业已经改完了，掌握的很不错，得了' + str(grade) + '分，加油~'
 
-    str0 = '下午好，和您反馈一下' + str(name) + '的物理学习情况：昨天学习的是比较重要的图像，'
+    str0 = '家长你好，和您反馈一下' + str(name) + '的物理学习情况：今天学习的是功和功率，'
     str3 = '^_^'
 
     str_all = str0 + str1 + str2 + str3
@@ -50,15 +50,21 @@ dfGrade = dfGrade.astype(int)
 dfWork['sum'] = dfGrade.apply(lambda x: x.sum(), axis=1)
 dfWork.rename(columns={'学生uin':'学员uid'},inplace=True)
 print(dfWork)
-file0 = input("1.请确保学生基本信息表与本程序在同一目录下2.输入工作簿的文件名（不用输入.xlsx）然后按Enter进入下一步")
-file_name = file0 + '.xlsx'
-times = input("3.请问反馈第几次的学习情况？输入阿拉伯数字后按Enter")
+# 写入
+path = os.path.dirname(os.path.abspath(__file__))
+output_file = os.path.join(path, 'dataWork.xlsx')
+dfWork.to_excel(output_file, index=False, engine="xlsxwriter")
+
+######
+#file0 = input("1.请确保学生基本信息表与本程序在同一目录下2.输入工作簿的文件名（不用输入.xlsx）然后按Enter进入下一步")
+# file_name = file0 + '.xlsx'
+# times = input("3.请问反馈第几次的学习情况？输入阿拉伯数字后按Enter")
 print('打开该目录下的文件：feedback.xlsx')
 
 # 读取 pd.read_excel(r'D:/source.xlsx', usecols='A:D,H')
-df = pd.read_excel(str(file_name))          # 读取数据源表
-df = df['直播时长'].astype('int')
-df['未提交作业次数'] = df.apply(lambda row: DiffValue(row['作业提交']), axis=1)
+df = pd.read_excel('./学情反馈解决方案/data.xlsx')          # 读取数据源表
+df = df['live'].astype('int')
+# df['未提交作业次数'] = df.apply(lambda row: DiffValue(row['作业提交']), axis=1)
 
 
 print(df)
@@ -66,13 +72,12 @@ print(df)
 # df_time = pd.read_excel('df_time.xlsx')     # 读取时间表
 # df_grade = pd.read_excel('df_grade.xlsx')   # 读取分数表
 # df = pd.merge(df,df_time.loc[:,['学号','核心课程第1节直播','核心课程第1节回放']],how='left',on = '学号') # vlookup 直播，回放时间
+df['live'].astype('int')
+# dfi = pd.merge(df, dfWork.loc[:, ['学员uid', 'sum']], how='left', on='学员uid')
+df['反馈'] = df.apply(lambda row:feedback(row['live'],row['sum'],row['真实姓名']),axis=1)
+# dfi['反馈1'] = dfi.apply(lambda row: salary(row['姓名'], row['未提交作业次数']), axis=1)
 
-dfi = pd.merge(df, dfWork.loc[:, ['学员uid', 'sum']], how='left', on='学员uid')
-dfi['反馈'] = dfi.apply(lambda row: feedback(
-    row['核心课程第' + str(times)+'节直播'], row['sum'], row['姓名']), axis=1)
-dfi['反馈1'] = dfi.apply(lambda row: salary(row['姓名'], row['未提交作业次数']), axis=1)
-# df[‘直播时长’].astype(‘int’)
-df2 = dfi[['学员uid', '姓名', '反馈', '反馈1']]
+df2 = df[['学员uid', '真实姓名', '反馈']]
 
 
 # 写入
